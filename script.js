312f66e205cc4af0f7767b1b54edf2965ca645bc
Thomas <ztom4122@gmail.com>
2024-10-09 21:26:50 +0200

    Refonte Total Et publication du mod

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-// scripts.js
+let currentIndex = 0;
+const images = document.querySelectorAll('.screenshot-gallery img');
 
-// Smooth scrolling pour les liens du menu
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+function showImages() {
+    const gallery = document.querySelector('.screenshot-gallery');
+    const width = images[0].clientWidth; // Largeur d'une image
+    gallery.style.transform = `translateX(${-currentIndex * width}px)`;
+}
+
+function nextImage() {
+    if (currentIndex < images.length - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0; // Recommencer au début
+    }
+    showImages();
+}
+
+function prevImage() {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = images.length - 1; // Aller à la dernière image
+    }
+    showImages();
+}
+
+window.addEventListener('resize', showImages); // Ajuste la vue lors du redimensionnement de la fenêtre
+
