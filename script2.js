61433837f56306b3a0034a61f8e7be4a5ed0c27f
Thomas <ztom4122@gmail.com>
2024-10-15 21:14:15 +0200

    Maintenance

---
+++ script2.js
@@ -0,0 +1,36 @@
+function copyIP() {
+    var copyText = document.getElementById("serverIP");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    alert("Adresse IP copiée : " + copyText.value);
+}
+
+let currentIndex = 0;
+const images = document.querySelectorAll('.screenshot-gallery img');
+
+function showImages() {
+    const gallery = document.querySelector('.screenshot-gallery');
+    const width = images[0].clientWidth; // Largeur d'une image
+    gallery.style.transform = `translateX(${-currentIndex * width}px)`;
+}
+
+function nextImage() {
+    if (currentIndex < images.length - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0; // Recommencer au début
+    }
+    showImages();
+}
+
+function prevImage() {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = images.length - 1; // Aller à la dernière image
+    }
+    showImages();
+}
+
+window.addEventListener('resize', showImages); // Ajuste la vue lors du redimensionnement de la fenêtre
